5222ae47b00c1977ca2c98e68ee1f16517ab94ff
demershow <demersontorres520@gmail.com>
2022-08-12 11:13:40 -0300

    aula 121 add: folder navigation

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -1,6 +1,13 @@
 class DropBoxController {
+
+
   constructor() {
+
+    this.currentFolder = ['Demer'];
+
     this.onselectionchange = new Event("selectionchange");
+
+    this.navEl = document.querySelector('#browse-location');
 
     this.btnSendFileEl = document.querySelector("#btn-send-file");
     this.inputFilesEl = document.querySelector("#files");
@@ -16,7 +23,11 @@ class DropBoxController {
 
     this.connectFirebase();
     this.initEvents();
-    this.readFiles();
+
+    this.openFolder();
+
+
+
   }
 
   connectFirebase() {
@@ -52,11 +63,29 @@ class DropBoxController {
       promises.push(this.ajax('/file', 'DELETE', formData));
 
     });
-    
+
     return Promise.all(promises);
   }
 
   initEvents() {
+
+    this.btnNewFolder.addEventListener('click', e => {
+
+      let name = prompt('Insira o nome da nova pasta: ');
+
+      if (name) {
+
+        this.getFirebaseRef().push().set({
+          name,
+          type: 'folder',
+          path: this.currentFolder.join('/')
+
+        });
+
+      }
+
+    })
+
     this.btnDelete.addEventListener("click", (e) => {
       this.removeTask()
         .then((responses) => {
@@ -64,7 +93,7 @@ class DropBoxController {
           responses.forEach(response => {
             if (response.fields.key) {
               this.getFirebaseRef().child
-              (response.fields.key).remove();
+                (response.fields.key).remove();
             }
           })
 
@@ -135,8 +164,11 @@ class DropBoxController {
     this.btnSendFileEl.disabled = false;
   }
 
-  getFirebaseRef() {
-    return firebase.database().ref("files");
+  getFirebaseRef(path) {
+
+    if (!path) path = this.currentFolder.join('/');
+
+    return firebase.database().ref(path);
   }
 
   modalShow(show = true) {
@@ -147,8 +179,8 @@ class DropBoxController {
     url,
     method = "GET",
     formData = new FormData(),
-    onprogress = function () {},
-    onloadstart = function () {}
+    onprogress = function () { },
+    onloadstart = function () { }
   ) {
     return new Promise((resolve, reject) => {
       let ajax = new XMLHttpRequest();
@@ -414,18 +446,102 @@ class DropBoxController {
   }
 
   readFiles() {
+
+    this.lastFolder = this.currentFolder.join('/');
+
     this.getFirebaseRef().on("value", (snapshot) => {
       this.listFilesEl.innerHTML = "";
       snapshot.forEach((snapshotItem) => {
         let key = snapshotItem.key;
         let data = snapshotItem.val();
 
-        this.listFilesEl.appendChild(this.getFileView(data, key));
+        if (data.type) {
+
+          this.listFilesEl.appendChild(this.getFileView(data, key));
+        }
+
+
       });
     });
   }
 
+  openFolder() {
+
+    if (this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
+
+    this.renderNav();
+    this.readFiles();
+
+
+  }
+
+  renderNav() {
+
+    let nav = document.createElement('nav');
+    let path = [];
+
+    for (let i = 0; i < this.currentFolder.length; i++) {
+
+      let folderName = this.currentFolder[i];
+      let span = document.createElement('span');
+
+      path.push(folderName);
+
+      if ((i + 1) === this.currentFolder.length) {
+
+        span.innerHTML = folderName;
+
+      } else {
+        span.className = 'breadcrumb-segment__wrapper';
+
+        span.innerHTML = `<span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+            <a href="#" data-path="${path.join('/')}"class="breadcrumb-segment">${folderName}</a>
+        </span>
+        <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+            <title>arrow-right</title>
+            <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+        </svg>`
+       
+      }
+
+      nav.appendChild(span);
+    }
+
+    this.navEl.innerHTML = nav.innerHTML;
+
+    this.navEl.querySelectorAll('a').forEach(a=>{
+
+      a.addEventListener('click', e=>{
+        e.preventDefault();
+
+        this.currentFolder = a.dataset.path.split('/');
+        this.openFolder();
+        
+      })
+
+    })
+
+  }
+
   initEventsLi(li) {
+
+    li.addEventListener('dblclick', (e) => {
+
+      let file = JSON.parse(li.dataset.file);
+
+      switch (file.type) {
+
+        case 'folder':
+          this.currentFolder.push(file.name);
+          this.openFolder();
+          break;
+
+        default:
+          window.open('file?path=' + file.path);
+      }
+
+    })
+
     li.addEventListener("click", (e) => {
       if (e.shiftKey) {
         let firstLi = this.listFilesEl.querySelector(".selected");
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,42 @@ var formidable = require('formidable')
 var fs = require('fs')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/file', (req, res) => {
+
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)) {
+    fs.readFile(path, (err, data) =>{
+
+      if(err){
+
+        console.error(err);
+        res.status(400).json({
+          error:err
+
+        })
+
+      } else {
+
+        res.status(200).end(data);
+      }
+
+    })
+
+
+  } else {
+
+    res.status(404).json({
+      error: 'File not found.'
+    })
+
+  }
+
+})
 
 router.delete('/file', (req, res) => {
   let form = new formidable.IncomingForm({
@@ -31,7 +64,7 @@ router.delete('/file', (req, res) => {
         }
       })
     }
-    
+
   })
 })
 
